44f4606dbcb5b3b2cead5e8e2b53ffcf2a84cca4
AounMuhammad <maoun982@gmail.com>
2026-04-28 09:52:05 +0500

    Remove PatientForms component from App

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ export default function App() {
         <About />
         <Testimonials />
         <FrequentlyAskedQuestions />
-        <PatientForms />
         <OfferSection />
         <BookingForm />
         <Contact />
